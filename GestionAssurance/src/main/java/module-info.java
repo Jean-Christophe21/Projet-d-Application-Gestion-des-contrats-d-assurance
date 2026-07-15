a68da1b52d6b578bc04a13b8223dc5832b73d46a
Jean Christophe <amevorjeanchristophe@gmail.com>
2026-07-15 13:37:04 +0000

    add: Projet Fxml GestionAssurance

---
+++ GestionAssurance/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module tg.univ.lome.epl.gestionassurance {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens tg.univ.lome.epl.gestionassurance to javafx.fxml;
+    exports tg.univ.lome.epl.gestionassurance;
+}
