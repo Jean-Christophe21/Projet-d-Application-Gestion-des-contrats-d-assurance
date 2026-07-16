72a0041127e424d741ed166f171622752b08ee84
Jean Christophe <amevorjeanchristophe@gmail.com>
2026-07-16 18:54:59 +0000

    add: formulaire fxml de login et le script sql de création des tables assure et contrats

---
+++ sql/tables.sql
@@ -0,0 +1,31 @@
+CREATE TABLE assure (
+id INT PRIMARY KEY,
+nom VARCHAR(50) NOT NULL,
+prenom VARCHAR(50) NOT NULL,
+dateNaiss DATE NOT NULL,
+contact VARCHAR(50) NOT NULL,
+adresse VARCHAR(50) NOT NULL,
+
+--les contraintes sur les éléments de la table assure
+CONSTRAINT CHK_date_naiss CHECK(dateNaiss  < date('now')),
+CONSTRAINT CHK_contact CHECK(contact GLOB '[0-9]*' AND LENGTH(contact) > 0)
+);
+
+
+CREATE TABLE contrats (
+
+contratNum VARCHAR(50) PRIMARY KEY,
+assureId INT NOT NULL,
+produit VARCHAR(20) NOT NULL,
+prime DECIMAL(10, 2) NOT NULL, 
+datedEffet DATE,
+statut  BOOLEAN NOT NULL,
+
+-- clé étrangère sur la table assure
+FOREIGN KEY (assureId) REFERENCES assure(id),
+
+-- les contraintes sur la table contrats
+CONSTRAINT CHK_prime CHECK(prime > 0),
+CONSTRAINT CHK_datedEffet CHECK(datedEffet  < date('now')),
+CONSTRAINT CHK_produit CHECK(produit IN ('BASE', 'MUTUELLE', 'PRIVEE_PARTICIPATION', 'PRIVEE_SANS_PARTICIPATION', 'REMBOURSEMENT', 'MIXTE'))
+);
